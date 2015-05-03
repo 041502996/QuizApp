b6dc54b97aac61fa8c90b29caf81ba9ca08fada9
Dillon O'Dwyer <041502996@my.central.wa.edu.au>
2015-05-04 06:21:31 +0800

    SQL Database
    .sql scripts to build the MySQL database and all relevant tables

---
+++ ASP Interface/SQL/create core.sql
@@ -0,0 +1,128 @@
+/* Database Creation Script
+   
+   Code begins by checking for if the database is currently created, 
+   if so drops it and re-creates it as a new, empty database.
+   
+   Code then generates tables completely void of Foreign Keys,
+   but still implementing other constraints.
+   
+   Finally code implements all Foreign Keys to tables.
+   */
+
+-- DROP DATABASE central_interface;
+CREATE DATABASE central_interface;
+
+USE central_interface;
+
+-- 'students' table holds information about students, including login details
+CREATE TABLE students
+	(student_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	student_email 		VARCHAR(30)	NOT NULL,
+	student_password 	VARCHAR(32)	NOT NULL,
+	student_name_first 	VARCHAR(25)	NOT NULL,
+	student_name_last 	VARCHAR(25)	NOT NULL,
+	student_verified 	VARCHAR(25)		NULL
+	);
+	
+-- 'lecturer' holds information about lecturers and superusers, including login details
+CREATE TABLE lecturers
+	(lecturer_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	lecturer_email 		VARCHAR(30)	NOT NULL,
+	lecturer_password 	VARCHAR(32)	NOT NULL,
+	lecturer_name_first VARCHAR(25)	NOT NULL,
+	lecturer_name_last 	VARCHAR(25)	NOT NULL,
+	lecturer_verified 	VARCHAR(25)		NULL,
+	lecturer_superuser 	BIT(1)		NOT NULL
+	);
+
+-- 'courses' table holds information about the courses
+-- Holds foreign key to 'lecturer' table
+CREATE TABLE courses
+	(course_id 			INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	course_coordinator 	INT			NOT NULL,
+	course_title 		VARCHAR(50)	NOT NULL,
+	course_abbreviation	VARCHAR(2)	NOT NULL
+	);
+	
+-- 'clusters' table holds information about the clusters
+CREATE TABLE clusters
+	(cluster_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	cluster_title 		VARCHAR(50)	NOT NULL
+	);
+
+/*
+RESOLVING MANY TO MANY CONNECTIONS
+*/
+
+-- 'course_enrolments' is an intermediary between 'courses' and 'students'
+-- Holds foreign keys to 'courses' table and 'students' table
+CREATE TABLE course_enrolments
+	(course_id 			INT		NOT NULL,
+	student_id 			INT		NOT NULL,
+	CONSTRAINT courses_students_key PRIMARY KEY(course_id, student_id)
+	);
+
+-- 'cluster_enrolments' is an intermediary between 'clusters' and 'students'
+-- Holds foreign keys to 'clusters' table and 'students' table
+CREATE TABLE cluster_enrolments
+	(cluster_id 		INT		NOT NULL,
+	student_id 			INT		NOT NULL,
+	CONSTRAINT clusters_students_key PRIMARY KEY(cluster_id, student_id)
+	);
+	
+-- 'course_clusters' is an intermediary between 'courses' and 'clusters'
+-- Holds foreign keys to 'courses' table and 'clusters' table
+CREATE TABLE course_clusters
+	(course_id 			INT		NOT NULL,
+	cluster_id 			INT		NOT NULL,
+	CONSTRAINT courses_clusters_key PRIMARY KEY(course_id, cluster_id)
+	);
+
+-- 'cluster_lecturers' is an intermediary between 'clusters' and 'lecturers'
+-- Holds foreign keys to 'clusters' table and 'lecturers' table
+CREATE TABLE cluster_lecturers
+	(cluster_id 		INT		NOT NULL,
+	lecturer_id 		INT		NOT NULL,
+	CONSTRAINT courses_clusters_key PRIMARY KEY(cluster_id, lecturer_id)
+	);
+
+/*
+RESOLVING FOREIGN KEY CONNECTIONS
+*/
+
+-- Add FK from 'course_enrolments' too 'students' and 'courses'
+ALTER TABLE course_enrolments
+	ADD CONSTRAINT course_enrolments_student_fk FOREIGN KEY (student_id)
+		REFERENCES students(student_id);
+ALTER TABLE course_enrolments
+	ADD CONSTRAINT course_enrolments_course_fk FOREIGN KEY (course_id)
+		REFERENCES courses(course_id);
+
+-- Add FK from 'cluster_enrolments' too 'clusters' and 'students'
+ALTER TABLE cluster_enrolments
+	ADD CONSTRAINT cluster_enrolments_student_fk FOREIGN KEY (student_id)
+		REFERENCES students(student_id);
+ALTER TABLE cluster_enrolments
+	ADD CONSTRAINT cluster_enrolments_cluster_fk FOREIGN KEY (cluster_id)
+		REFERENCES clusters(cluster_id);
+
+-- Add FK from 'course_clusters' too 'courses' and 'clusters'
+ALTER TABLE course_clusters
+	ADD CONSTRAINT course_clusters_course_fk FOREIGN KEY (course_id)
+		REFERENCES courses(course_id);
+ALTER TABLE course_clusters
+	ADD CONSTRAINT course_clusters_cluster_fk FOREIGN KEY (cluster_id)
+		REFERENCES clusters(cluster_id);
+
+-- Add FK from 'cluster_lecturers' too 'cluster' and 'lecturers'
+ALTER TABLE cluster_lecturers
+	ADD CONSTRAINT cluster_lecturers_cluster_fk FOREIGN KEY (cluster_id)
+		REFERENCES clusters(cluster_id);
+ALTER TABLE cluster_lecturers
+	ADD CONSTRAINT cluster_lecturers_lecturer_fk FOREIGN KEY (lecturer_id)
+		REFERENCES lecturers(lecturer_id);
+		
+-- Add FK from 'courses' too 'lecturers'
+ALTER TABLE courses
+	ADD CONSTRAINT courses_coordinator_fk FOREIGN KEY (course_coordinator)
+		REFERENCES lecturers(lecturer_id);
---
+++ ASP Interface/SQL/create quiz.sql
@@ -0,0 +1,170 @@
+/* Database Creation Script
+   
+   Code begins by checking for if the database is currently created, 
+   if so drops it and re-creates it as a new, empty database.
+   
+   Code then generates tables completely void of Foreign Keys,
+   but still implementing other constraints.
+   
+   Finally code implements all Foreign Keys to tables.
+   */
+
+USE central_interface;
+
+-- 'quizzes' table holds information about quiz in general
+-- Holds foreign key to 'lecturer' and 'cluster' table
+CREATE TABLE quizzes
+	(quiz_id 			INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	quiz_cluster 		INT			NOT NULL,
+	quiz_creator 		INT			NOT NULL,
+	quiz_title 			VARCHAR(50)	NOT NULL,
+    quiz_timer			INT				NULL,
+	quiz_due_date 		DATETIME	NOT NULL,
+	quiz_creation_date 	DATETIME		NULL
+	);
+	
+-- 'questions' table holds information about question format and order
+-- Holds foreign key to 'quizzes' and 'format' table
+CREATE TABLE questions
+	(question_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	quiz_id 			INT			NOT NULL,
+	question_format 	INT			NOT NULL,
+	question_position 	INT			NOT NULL
+	);
+
+-- 'formats' table holds html information about question formats
+CREATE TABLE formats
+	(format_id 			INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	format_title 		VARCHAR(25)	NOT NULL,
+	format_abbreviation VARCHAR(2)	NOT NULL,
+	format_html_review	TEXT		NOT NULL,
+    format_html_create	TEXT		NOT NULL
+	);
+
+/*
+CREATING DIFFERENT QUESTION TABLES
+*/
+
+-- MULTIPLE CHOICE requires two tables
+-- 'multiple_choice' table holds all multiple choice questions, and correct answer
+-- Holds foreign key to 'questions' table
+CREATE TABLE multiple_choice
+	(mc_id 				INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	mc_question_id 		INT			NOT NULL,
+    mc_question			VARCHAR(75)	NOT NULL,
+    mc_answer			VARCHAR(25)	NOT NULL
+	);
+-- 'mc_wrong' table holds all multiple choice incorrect answers
+-- Holds foreign key to 'multiple_choice' table
+CREATE TABLE mc_wrong
+	(mc_wrong_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	mc_id 				INT			NOT NULL,
+    mc_wrong_answer		VARCHAR(25)	NOT NULL
+	);
+    
+-- MULTIPLE ANSWER requires three tables
+-- 'multiple_answer' table holds all multiple answer questions, with no answers
+-- Holds foreign key to 'questions' table
+CREATE TABLE multiple_answer
+	(ma_id 				INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	ma_question_id 		INT			NOT NULL,
+    ma_question			VARCHAR(75)	NOT NULL
+	);
+-- 'ma_wrong' table holds all multiple answer incorrect answers
+-- Holds foreign key to 'multiple_answer' table
+CREATE TABLE ma_wrong
+	(ma_wrong_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	ma_id 				INT			NOT NULL,
+    ma_wrong_answer		VARCHAR(25)	NOT NULL
+	);
+-- 'ma_correct' table holds all multiple answer correct answers
+-- Holds foreign key to 'multiple_answer' table
+CREATE TABLE ma_correct
+	(ma_correct_id 		INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	ma_id 				INT			NOT NULL,
+    ma_correct_answer	VARCHAR(25)	NOT NULL
+	);
+    
+-- TRUE/FALSE requires one table
+-- 'true_false' table holds all true/false questions
+-- Holds foreign key to 'questions' table
+CREATE TABLE true_false
+	(tf_id 				INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	tf_question_id 		INT			NOT NULL,
+    tf_question			VARCHAR(75)	NOT NULL,
+    tf_answer			BIT(1)		NOT NULL
+	);
+
+-- LONG RESPONSE requires one table
+-- 'long_response' table holds all long answer questions
+-- Holds foreign key to 'questions' table
+CREATE TABLE long_response
+	(long_id 			INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	long_question_id 	INT			NOT NULL,
+    long_question		VARCHAR(75)	NOT NULL
+	);
+
+-- SHORT RESPONSE requires one table
+-- 'short_response' table holds all short answer questions
+-- Holds foreign key to 'questions' table
+CREATE TABLE short_response
+	(short_id 			INT			NOT NULL	PRIMARY KEY AUTO_INCREMENT,
+	short_question_id 	INT			NOT NULL,
+    short_question		VARCHAR(75)	NOT NULL
+	);
+
+/*
+RESOLVING FOREIGN KEY CONNECTIONS
+*/
+-- Add FK from 'quizzes' too 'clusters' and 'lecturers'
+ALTER TABLE quizzes
+	ADD CONSTRAINT quizzes_cluster_fk FOREIGN KEY (quiz_cluster)
+		REFERENCES clusters(cluster_id);
+ALTER TABLE quizzes
+	ADD CONSTRAINT quizzes_lecturer_fk FOREIGN KEY (quiz_creator)
+		REFERENCES lecturers(lecturer_id);
+
+-- Add FK from 'questions' too 'quizzes' and 'formats'
+ALTER TABLE questions
+	ADD CONSTRAINT questions_quizzes_fk FOREIGN KEY (quiz_id)
+		REFERENCES quizzes(quiz_id);
+ALTER TABLE questions
+	ADD CONSTRAINT questions_formats_fk FOREIGN KEY (question_format)
+		REFERENCES formats(format_id);
+
+
+-- HANDLING QUESTION TABLE FOREIGN KEYS
+
+-- Add FK from 'mc_wrong' too 'multiple_choice'
+ALTER TABLE mc_wrong
+	ADD CONSTRAINT mc_wrong_multiple_choice_fk FOREIGN KEY (mc_id)
+		REFERENCES multiple_choice(mc_id);
+-- Add FK from 'multiple_choice' too 'questions'
+ALTER TABLE multiple_choice
+	ADD CONSTRAINT multiple_choice_questions_fk FOREIGN KEY (mc_question_id)
+		REFERENCES questions(question_id);
+
+-- Add FK from 'ma_wrong' and 'ma_correct' too 'multiple_answer'
+ALTER TABLE ma_correct
+	ADD CONSTRAINT ma_correct_multiple_answer_fk FOREIGN KEY (ma_id)
+		REFERENCES multiple_answer(ma_id);
+ALTER TABLE ma_wrong
+	ADD CONSTRAINT ma_wrong_multiple_answer_fk FOREIGN KEY (ma_id)
+		REFERENCES multiple_answer(ma_id);
+-- Add FK from 'multiple_answer' too 'questions'
+ALTER TABLE multiple_answer
+	ADD CONSTRAINT multiple_answer_questions_fk FOREIGN KEY (ma_question_id)
+		REFERENCES questions(question_id);
+
+-- Add FK from 'true_false' too 'questions'
+ALTER TABLE true_false
+	ADD CONSTRAINT true_false_questions_fk FOREIGN KEY (tf_question_id)
+		REFERENCES questions(question_id);
+-- Add FK from 'short_response' too 'questions'
+ALTER TABLE short_response
+	ADD CONSTRAINT short_response_questions_fk FOREIGN KEY (short_question_id)
+		REFERENCES questions(question_id);
+-- Add FK from 'long_response' too 'questions'
+ALTER TABLE long_response
+	ADD CONSTRAINT long_response_questions_fk FOREIGN KEY (long_question_id)
+		REFERENCES questions(question_id);
